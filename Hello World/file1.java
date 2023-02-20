fb56f90e78fd9a2b9fa3859e7f638cecceed06c2
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-20 20:21:48 +0500

    Create file1.java

---
+++ Hello World/file1.java
@@ -0,0 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //System.out.println(Hello World); // this gives error
+    }
+}
